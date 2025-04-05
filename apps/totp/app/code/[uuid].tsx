5a95273cd2717ceab4c348555e61c5bfd9672ae0
Joe Grigg <joe@ghost.org>
2025-04-05 17:51:46 +0100

    Added base code page styles

--- apps/totp/app/code/[uuid].tsx
+++ apps/totp/app/code/[uuid].tsx
@@ -1,9 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Header } from 'ui';
 
 export default function() {
   return (
-    <View>
+    <View style={styles.container}>
+      <Header
+        title="Code"
+        icon="trash"
+        iconLeft="chevron-left"
+        iconLeftLink="/"
+      />
       <Text>Code</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
