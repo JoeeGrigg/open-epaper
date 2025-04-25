3b3e7a8964edc57da521e94a949404c5aab2404d
Joe Grigg <joe@ghost.org>
2025-04-25 17:56:46 +0100

    Added list select modal

--- apps/reminders/app/index.tsx
+++ apps/reminders/app/index.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import { View } from 'react-native';
-import { Header } from 'ui';
+import { ButtonModal, Header } from 'ui';
 
 export default function Index() {
   const [list, setList] = useState<string | null>(null);
+  const [listsVisible, setListsVisible] = useState(false);
+
+  const optionButtons = [
+    { text: 'All', onPress: () => setList('all') },
+    { text: 'Today', onPress: () => setList('today') },
+    { text: 'This Week', onPress: () => setList('thisWeek') },
+    { text: 'This Month', onPress: () => setList('thisMonth') },
+  ];
 
   return (
     <View
@@ -13,7 +21,9 @@ export default function Index() {
         title={list || 'Reminders'}
         icon="plus"
         iconLeft="bars"
+        iconLeftOnPress={() => setListsVisible(true)}
       />
+      <ButtonModal buttons={optionButtons} visible={listsVisible} onRequestClose={() => setListsVisible(false)} />
     </View>
   );
 }
